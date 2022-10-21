cfef1925b8f32b34a446e84b83b772795827c7be
milena <milenakalo@icloud.com>
2022-10-21 17:02:56 +0200

    Schnittstelle verbessert

--- src/main/java/org/groupone/Schnittstelle.java
+++ src/main/java/org/groupone/Schnittstelle.java
@@ -1,84 +1,90 @@
 package org.groupone;
 
+import java.util.List;
+
 public interface Schnittstelle {
 
     /**
-     *Erstelle Spiel
+     *Erstellt eine zufällige Reihenflge in der die Spieler zum Zug kommen
+     * (Methode wird der Klasse Spiel zugeorndet)
+     * @return  Liste der Reihenfolge der Spieler
      */
-     void erstelleSpiel();
+    List<Spieler> erstelleSpielerReihenfolge();
 
     /**
-     *Erstelle Spielerreihenfolge
-     */
-    void erstelleSpielerReihenfolge();
-
-    /**
-     *Jeder Spieler erhält 7 Karten am Anfang
+     * Jeder Spieler der am Spiel teilnimmt erhält 5 Karten am Anfang
+     * (Methode der KLasse Spiel)
      */
     void gebeKartenAusAmAnfang();
 
     /**
-     *Oberste Karte Ziehstapel geöffnet
+     * Oberste Karte des Ziehstapels wird geöffnet
+     * (Methode der KLasse Spiel)
      */
     void ersteKarteAufBrett();
 
     /**
-     *Spieler können Karten ablegen gleiche Farbe oder Wert
+     *Spieler können Karten ablegen die die gleiche Farbe oder Wert haben
+     * @param karte die Karte die abgelegt wird
      */
-    public void legeKarteab();
+    void legeKarteAb(Karte karte);
 
     /**
-     * Spieler muss Karte ziehen weil keine passende
+     * Spieler muss Karte ziehen weil er keine passende Karte mehr hat.
+     * Es wurde eine 7 gelegt: nächster Spieler der keine passende Karte hat muss 2 ziehen.
+     * Wird auf eine gelegte Sieben noch einmal 7 gelegt muss der Spieler der keine
+     * passende Karte hat 4 Karten ziehen.
+     * @param spieler der Spieler der in seiner Spielerhand eine Karte hinzugefügt bekommt
+     * @param ziehStapel der Stapel an Karten wo eine Karte gezogen wird
      */
-    void zieheKarte();
+    void zieheKarte(Spieler spieler, ZiehStapel ziehStapel);
 
     /**
      * Nächster Spieler dran
+     * @param spiel Spiel wo der nächste Spieler in der Liste genommen wird
+     * @return der Spieler der nun an der Reihe ist
      */
-     void nächsterSpielerIstDran();
+     Spieler nächsterSpielerIstDran(Spiel spiel);
+
     /**
-     * Vorletzte Karte Mau sagen
+     * bei der vorletzten Karte muss Mau gesagt werden
      */
     void sageMau();
 
     /**
-     * Mau vergessen dann 2 Strafkarten ziehen
+     * Der Spieler hat vergessen Mau zu sagen und muss jetzt 2 Strafkarten ziehen
+     * @param spieler Spieler der zwei Karten ziehen muss
+     * @param ziehStapel Stapel aus dem 2 Karten gezogen werden
      */
-     void mauStrafe();
+     void mauStrafe(Spieler spieler, ZiehStapel ziehStapel);
 
     /**
-     * Letzte Karte abgelegt Mau mau sagen
+     * Die letzte Karte wurd abgelegt und der Spieler muss MauMau sagen.
      */
     void maumau();
 
     /**
-     * 7 gelegt: nächster Spieler muss 2 ziehen
-     * Wieder auf 7 7 gelegt nächste Spieler muss 4 ziehen
+     * Karten werden gemischt
+     * @param karteList Liste aus Karten die gemischt werden sollen
+     * @return die fertig gemischte Kartenliste
      */
-     void siebenGelegt();
-
-    /**
-     * Bube: Farbwunsch
-     */
-    void bubeGelegt();
-
-    /**
-     * Ablagestapel mischt Karten und gibt an Deck ab
-     */
-     void gibAnDeckZurück();
+     List<Karte> mischeKarten(List<Karte> karteList );
 
     /**
      * Spiel wird beendet
+     * (Methode die in der KLasse Spiel angesiedelt wird)
      */
      void beendeSpiel();
 
     /**
-     * addiere Punkte auf
+     * Spieler bekommt einen Punkt dafür dass er gewonnen hat dazu.
+     * (Methode die der Klasse Spieler gehört)
      */
-     void punkte();
+     void erhöhePunkt();
 
     /**
-     * zeigt den Gewinner
+     * zeigt den Gewinner des Spiels an
+     * @param spieler der Spieler der gewonnen hat
      */
-     void gibGewinneraus();
+     void gibGewinneraus(Spieler spieler);
 }
