10edd3f27de05023a594339c820aac9877e9ad7a
milena <milenakalo@icloud.com>
2022-12-13 10:27:09 +0100

    refactor

--- spieler_management/src/main/java/implementation/SpielerImpl.java
+++ spieler_management/src/main/java/implementation/SpielerImpl.java
@@ -9,22 +9,26 @@ public class SpielerImpl implements SpielerService {
 
     @Override
     public void erhöhePunkt(Spieler spieler) {
-
+        spieler.setPunkte(spieler.getPunkte() + 1);
     }
 
     @Override
     public void maumau(Spieler spieler) {
-
+        spieler.setMauGesagt(true);
     }
 
     @Override
     public void sageMau(Spieler spieler) {
+        boolean b = spieler.getSpielerHand().getAnzahlKarten() <= 1;
 
+        if (b == true) {
+            spieler.setMauGesagt(true);
+        }
     }
 
     @Override
     public void mauZuruecksetzen(Spieler spieler) {
-
+        spieler.setMauGesagt(false);
     }
 
 
